d5d8906bf201d825cc885d1d1a583096165a209d
Alan Kligman <ack@mozilla.com>
2013-08-08 18:09:23 -0400

    Not needed.

--- src/create-session-description-observer.cc
+++
@@ -1,8 +0,0 @@
-#include "create-session-description-observer.h"
-
-void CreateSessionDescriptionObserver::OnSuccess( webrtc::SessionDescriptionInterface* sdp ) {
-  parent->OnCreateSessionDescriptionSuccess( sdp );
-}
-void CreateSessionDescriptionObserver::OnFailure( const std::string& msg ) {
-  parent->OnCreateSessionDescriptionFailure( msg );
-}
