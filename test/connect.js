6f46f206864b20e8b18001ddc591d57a06870a3f
Damon Oehlman <damon.oehlman@gmail.com>
2013-12-18 13:42:10 +1100

    Finer grained connection test to see where things are falling down

--- test/connect.js
+++ test/connect.js
@@ -1,8 +1,9 @@
 var test = require('tape');
 var RTCPeerConnection = require('../peerconnection');
+var RTCDataChannel = require('../datachannel');
 var peers = [];
 var dcs = [];
-var peerpair = require('peerpair');
+var localDesc;
 
 test('create the peer connections', function(t) {
   t.plan(2);
@@ -21,37 +22,75 @@ test('peers are created and in the expected connection state', function(t) {
   t.equal(peers[1].iceConnectionState, 'new');
 });
 
-// test('can create a datachannel', function(t) {
-//   t.plan(2);
-//   t.ok(dcs[0] = peers[0].createDataChannel('test'));
-//   t.equal(dcs[0].label, 'test');
-// });
-
-test('pair the connections', function(t) {
-  t.plan(1);
-  peers = peerpair(peers, {
-    RTCSessionDescription: require('../sessiondescription')
-  });
-  t.pass('paired');
-});
-
-test('can connect the peers', function(t) {
+test('create a datachannel on peer:0', function(t) {
   t.plan(2);
-
-  peers.events.once('connected', function() {
-    t.equal(peers[0].iceConnectionState, 'connected');
-    t.equal(peers[1].iceConnectionState, 'connected');
-  });
-
-  peers.connect();
+  t.ok(dcs[0] = peers[0].createDataChannel('test'));
+  t.equal(dcs[0].label, 'test', 'created with correct label');
 });
 
-// test('release the connection references', function(t) {
-//   peers = [];
-// });
+test('createOffer for peer:0', function(t) {
 
-// test('close the connections', function(t) {
-//   t.plan(2);
-//   peers[0].close();
-//   peers[1].close();
-// });
+  var fail = t.ifError.bind(t);
+
+  function pass(desc) {
+    // save the local description
+    localDesc = desc;
+
+    // run the checks
+    t.ok(desc, 'createOffer succeeded');
+    t.equal(desc.type, 'offer', 'type === offer');
+    t.ok(desc.sdp, 'got sdp');
+  }
+
+  t.plan(3);
+  peers[0].createOffer(pass, fail);
+});
+
+test('setLocalDescription for peer:0', function(t) {
+  var fail = t.ifError.bind(t);
+  var pass = t.pass.bind(t, 'ok');
+
+  t.plan(1);
+  peers[0].setLocalDescription(localDesc, pass, fail);
+});
+
+test('setRemoteDescription for peer:1', function(t) {
+  var fail = t.ifError.bind(t);
+  var pass = t.pass.bind(t, 'ok');
+
+  t.plan(1);
+  peers[1].setRemoteDescription(peers[0].localDescription, pass, fail);
+});
+
+test('createAnswer for peer:1', function(t) {
+  var fail = t.ifError.bind(t);
+
+  function pass(desc) {
+    // save the local description
+    localDesc = desc;
+
+    // run the checks
+    t.ok(desc, 'createOffer succeeded');
+    t.equal(desc.type, 'answer', 'type === answer');
+    t.ok(desc.sdp, 'got sdp');
+  }
+
+  t.plan(3);
+  peers[1].createAnswer(pass, fail);
+});
+
+test('setLocalDescription for peer:1', function(t) {
+  var fail = t.ifError.bind(t);
+  var pass = t.pass.bind(t, 'ok');
+
+  t.plan(1);
+  peers[1].setLocalDescription(localDesc, pass, fail);
+});
+
+test('setRemoteDescription for peer:0', function(t) {
+  var fail = t.ifError.bind(t);
+  var pass = t.pass.bind(t, 'ok');
+
+  t.plan(1);
+  peers[0].setRemoteDescription(peers[1].localDescription, pass, fail);
+});
