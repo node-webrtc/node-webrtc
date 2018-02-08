83b6edea5818b721cc5f1856d6a4bbe1c7313c3c
Mark Andrus Roberts <markandrusroberts@gmail.com>
2018-02-08 10:58:07 -0800

    Update Copyright

--- src/set-remote-description-observer.h
+++ src/set-remote-description-observer.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017 The node-webrtc project authors. All rights reserved.
+/* Copyright (c) 2018 The node-webrtc project authors. All rights reserved.
  *
  * Use of this source code is governed by a BSD-style license that can be found
  * in the LICENSE.md file in the root of the source tree. All contributing
