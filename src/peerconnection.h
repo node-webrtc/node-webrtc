4077487ce98c698d31dadbd95d4de71ef697da26
Emmet McPoland <emcpoland01@qub.ac.uk>
2018-06-14 12:24:36 +0100

    Fixed issue with previous commit

--- src/peerconnection.h
+++ src/peerconnection.h
@@ -172,7 +172,7 @@ class PeerConnection
         ICE_GATHERING_STATE_CHANGE
   };
 
-  explicit PeerConnection(webrtc::PeerConnectionInterface::IceServers iceServerList, bool enableDtlSrtp);
+  explicit PeerConnection(webrtc::PeerConnectionInterface::IceServers iceServerList, rtc::Optional<bool> enableDtlsSrtp);
   ~PeerConnection();
 
   //
