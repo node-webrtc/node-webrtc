38fe6ab975db9f8cbbd400705625c5f2966c630b
nokta <doganmurat@gmail.com>
2018-11-23 14:33:39 +0300

    Add support for cross compile arm

--- scripts/download-webrtc.sh
+++ scripts/download-webrtc.sh
@@ -17,6 +17,9 @@ else
   python src/build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
 fi
 
+# Install deps
+src/build/install-build-deps.sh
+
 python src/tools/clang/scripts/update.py
 
 rm -f webrtc
