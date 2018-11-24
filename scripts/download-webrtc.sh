ca22f3d03154705c35a742b094ecadebc5745f90
Murat Dogan <doganmurat@gmail.com>
2018-11-24 15:23:59 +0300

    Delete extra install-sysroot.py command
    We can definitely dlete this command from here. 
    python src/build/linux/sysroot_scripts/install-sysroot.py --arch=amd64

--- scripts/download-webrtc.sh
+++ scripts/download-webrtc.sh
@@ -14,8 +14,6 @@ if [ "${PLATFORM}" = darwin ]; then
   download_from_google_storage --no_resume --platform=${PLATFORM} --no_auth --bucket chromium-gn -s src/buildtools/mac/gn.sha1
 else
   download_from_google_storage --no_resume --platform=${PLATFORM} --no_auth --bucket chromium-gn -s src/buildtools/linux64/gn.sha1
-  # TODO(mroberts): See if we can remove this.
-  python src/build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
 fi
 
 python src/tools/clang/scripts/update.py
