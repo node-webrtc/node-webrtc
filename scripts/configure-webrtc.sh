311b24a813c260f5a0c8d1e0c28d49451dd410e8
zsmweb <zsmweb@gmail.com>
2018-12-14 16:55:46 +0800

    Support arm64 (#459)

--- scripts/configure-webrtc.sh
+++ scripts/configure-webrtc.sh
@@ -11,7 +11,10 @@ cd ${SOURCE_DIR}
 if [ "$TARGET_ARCH" == "arm" ]; then
   python build/linux/sysroot_scripts/install-sysroot.py --arch=arm
 else
-  python build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
+  if [ "$TARGET_ARCH" == "arm64" ]; then
+    python build/linux/sysroot_scripts/install-sysroot.py --arch=arm64
+  else
+    python build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
+  fi
 fi
-
 gn gen ${BINARY_DIR} "--args=${GN_GEN_ARGS}"
