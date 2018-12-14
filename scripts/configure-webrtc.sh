655e3f161ac728c38a35c17041e6a85bd47e538c
Mark Roberts <markandrusroberts@gmail.com>
2018-12-14 16:42:16 +0800

    Followup on #459

--- scripts/configure-webrtc.sh
+++ scripts/configure-webrtc.sh
@@ -10,11 +10,9 @@ cd ${SOURCE_DIR}
 
 if [ "$TARGET_ARCH" == "arm" ]; then
   python build/linux/sysroot_scripts/install-sysroot.py --arch=arm
+elif [ "$TARGET_ARCH" == "arm64" ]; then
+  python build/linux/sysroot_scripts/install-sysroot.py --arch=arm64
 else
-  if [ "$TARGET_ARCH" == "arm64" ]; then
-    python build/linux/sysroot_scripts/install-sysroot.py --arch=arm64
-  else
-    python build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
-  fi
+  python build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
 fi
 gn gen ${BINARY_DIR} "--args=${GN_GEN_ARGS}"
