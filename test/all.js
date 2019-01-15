b9886f39f0f43b123a1a58f4c1b2b2a9d5c349ab
Mark Roberts <markandrusroberts@gmail.com>
2019-01-15 12:22:51 -0800

    Alphabetize tests in test/all.js

--- test/all.js
+++ test/all.js
@@ -2,31 +2,37 @@
 
 const semver = require('semver');
 
-require('./pass-interface-to-method');
-require('./create-offer');
-require('./sessiondesc');
+require('./closing-data-channel');
+require('./closing-peer-connection');
 require('./connect');
+require('./create-offer');
+require('./get-configuration');
+require('./i420helpers');
 require('./iceservers');
-// require('./bwtest').tape();
 require('./mediastream');
-// NOTE(mroberts): https://github.com/feross/simple-peer/pull/355 introduced
-// syntax incompatible with Node 6.
+require('./pass-interface-to-method');
+require('./rtcdatachannel');
+require('./rtcrtpreceiver');
+require('./rtcvideosink');
+require('./rtcvideosource');
+require('./send-arraybuffer');
+require('./sessiondesc');
+
+// TODO(mroberts): These two tests use simple-peer, which introduced syntax
+// incompatible with Node 6 in
+//
+//   https://github.com/feross/simple-peer/pull/355 introduced
+//
+// Therefore, we skip them. Remove this if-statement once we drop support for
+// Node 6.
 if (semver(process.version).major > 6) {
   require('./multiconnect');
   require('./custom-settings');
 }
-require('./closing-peer-connection');
-require('./closing-data-channel');
-require('./get-configuration');
-require('./rtcrtpreceiver');
-require('./send-arraybuffer');
-require('./rtcdatachannel');
 
-if (semver(process.version).major >= 9) {
-  require('./i420helpers');
-  require('./rtcvideosink');
-  require('./rtcvideosource');
-  if (typeof gc === 'function') {
-    require('./destructor');
-  }
+// TODO(mroberts): async_hooks were introduced in Node 9. We use them to test
+// that destructors fire at the appropriate time (and hence, no memory leaks
+// occur). Once we drop support for Node < 9, remove this.
+if (semver(process.version).major >= 9 && typeof gc === 'function') {
+  require('./destructor');
 }
