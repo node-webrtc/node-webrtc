65f2742aac25f1add572606bcbe3c47026cbbe58
Mark Roberts <markandrusroberts@gmail.com>
2019-01-18 22:40:24 -0800

    Fix test/rtcvideosink.js for Node 6

--- test/rtcvideosink.js
+++ test/rtcvideosink.js
@@ -5,18 +5,19 @@ const test = require('tape');
 const { RTCVideoSink, RTCVideoSource } = require('..');
 const { I420Frame } = require('./lib/frame');
 
-test('RTCVideoSink', async t => {
+test('RTCVideoSink', t => {
   const source = new RTCVideoSource();
   const track = source.createTrack();
   const sink = new RTCVideoSink(track);
   const inputFrame = new I420Frame(160, 120);
   const outputFramePromise = new Promise(resolve => { sink.onframe = resolve; });
   source.onFrame(inputFrame);
-  const outputFrame = await outputFramePromise;
-  t.equal(inputFrame.width, outputFrame.width);
-  t.equal(inputFrame.height, outputFrame.height);
-  t.deepEqual(inputFrame.data, outputFrame.data);
-  sink.stop();
-  track.stop();
-  t.end();
+  return outputFramePromise.then(outputFrame => {
+    t.equal(inputFrame.width, outputFrame.width);
+    t.equal(inputFrame.height, outputFrame.height);
+    t.deepEqual(inputFrame.data, outputFrame.data);
+    sink.stop();
+    track.stop();
+    t.end();
+  });
 });
