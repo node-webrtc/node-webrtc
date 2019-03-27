b9755d1cf75ee06aa251d8f3773ca89fccb869f2
Mark Roberts <markandrusroberts@gmail.com>
2019-03-27 21:12:14 +0800

    Refactor CreateSessionDescriptionObserver

--- src/createsessiondescriptionobserver.cc
+++ src/createsessiondescriptionobserver.cc
@@ -7,7 +7,6 @@
  */
 #include "src/createsessiondescriptionobserver.h"
 
-#include <webrtc/api/jsep.h>
 #include <webrtc/api/rtc_error.h>
 
 #include "src/converters.h"
@@ -15,27 +14,58 @@
 #include "src/error.h"
 #include "src/peerconnection.h"
 
-void node_webrtc::CreateSessionDescriptionObserver::OnSuccess(webrtc::SessionDescriptionInterface* sdp) {
-  if (_promise) {
-    auto validation = node_webrtc::From<RTCSessionDescriptionInit>(sdp);
-    if (validation.IsInvalid()) {
-      _promise->Reject(node_webrtc::SomeError(validation.ToErrors()[0]));
-    } else {
-      auto description = validation.UnsafeFromValid();
-      parent->SaveLastSdp(description);
-      _promise->Resolve(description);
-    }
-    parent->Dispatch(std::move(_promise));
-  }
-  delete sdp;
+node_webrtc::CreateSessionDescriptionObserver::CreateSessionDescriptionObserver(
+    node_webrtc::PeerConnection* peer_connection)
+  : _peer_connection(peer_connection) {
+  Nan::HandleScope scope;
+  _resolver = std::make_unique<Nan::Persistent<v8::Promise::Resolver>>(
+          v8::Promise::Resolver::New(Nan::GetCurrentContext()).ToLocalChecked());
 }
 
-void node_webrtc::CreateSessionDescriptionObserver::OnFailure(const webrtc::RTCError error) {
-  if (_promise) {
-    auto someError = node_webrtc::From<node_webrtc::SomeError>(&error).FromValidation([](node_webrtc::Errors errors) {
-      return node_webrtc::SomeError(errors[0]);
-    });
-    _promise->Reject(someError);
-    parent->Dispatch(std::move(_promise));
-  }
+node_webrtc::CreateSessionDescriptionObserver::CreateSessionDescriptionObserver(
+    node_webrtc::PeerConnection* peer_connection,
+    v8::Local<v8::Promise::Resolver> resolver)
+  : _peer_connection(peer_connection) {
+  Nan::HandleScope scope;
+  _resolver = std::make_unique<Nan::Persistent<v8::Promise::Resolver>>(resolver);
+}
+
+void node_webrtc::CreateSessionDescriptionObserver::OnSuccess(webrtc::SessionDescriptionInterface* description) {
+  auto validation = node_webrtc::From<RTCSessionDescriptionInit>(description);
+  delete description;
+  _peer_connection->Dispatch(node_webrtc::CreateCallback<node_webrtc::PeerConnection>(
+  [peer_connection = _peer_connection, _resolver = std::move(_resolver), validation]() {
+    Nan::HandleScope scope;
+    v8::Local<v8::Promise::Resolver> resolver = Nan::New(*_resolver);
+
+    if (validation.IsInvalid()) {
+      node_webrtc::SomeError error(validation.ToErrors()[0]);
+      CONVERT_OR_REJECT_AND_RETURN(resolver, error, value, v8::Local<v8::Value>);
+      resolver->Reject(Nan::GetCurrentContext(), value).IsNothing();
+    } else {
+      auto description = validation.UnsafeFromValid();
+      peer_connection->SaveLastSdp(description);
+      CONVERT_OR_REJECT_AND_RETURN(resolver, description, value, v8::Local<v8::Value>);
+      resolver->Resolve(Nan::GetCurrentContext(), value).IsNothing();
+    }
+  }));
+}
+
+void node_webrtc::CreateSessionDescriptionObserver::OnFailure(webrtc::RTCError error) {
+  auto someError = node_webrtc::From<node_webrtc::SomeError>(&error).FromValidation([](node_webrtc::Errors errors) {
+    return node_webrtc::SomeError(errors[0]);
+  });
+  _peer_connection->Dispatch(node_webrtc::CreateCallback<node_webrtc::PeerConnection>(
+  [_resolver = std::move(_resolver), someError]() {
+    Nan::HandleScope scope;
+    v8::Local<v8::Promise::Resolver> resolver = Nan::New(*_resolver);
+    CONVERT_OR_REJECT_AND_RETURN(resolver, someError, value, v8::Local<v8::Value>);
+    resolver->Reject(Nan::GetCurrentContext(), value).IsNothing();
+  }));
+}
+
+v8::Local<v8::Promise> node_webrtc::CreateSessionDescriptionObserver::promise() {
+  Nan::EscapableHandleScope scope;
+  v8::Local<v8::Promise::Resolver> resolver = Nan::New(*_resolver);
+  return scope.Escape(resolver->GetPromise());
 }
