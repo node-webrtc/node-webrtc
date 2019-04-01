1ff7eb74d80f8452bf64f45a0e5e8695a26615d7
Mark Roberts <markandrusroberts@gmail.com>
2019-04-01 10:49:10 +0800

    Change MEDIA_TYPE to CRICKET_MEDIA_TYPE

--- src/enums/webrtc/media_type.cc
+++ src/enums/webrtc/media_type.cc
@@ -1,5 +1,5 @@
 #include "src/enums/webrtc/media_type.h"
 
-#define ENUM(X) MEDIA_TYPE ## X
+#define ENUM(X) CRICKET_MEDIA_TYPE ## X
 #include "src/enums/macros/impls.h"
 #undef ENUM
--- src/enums/webrtc/media_type.h
+++ src/enums/webrtc/media_type.h
@@ -5,13 +5,13 @@
 // IWYU pragma: no_include "src/enums/macros/impls.h"
 
 // FIXME(mroberts): I'm not sure that "data" should be valid.
-#define MEDIA_TYPE cricket::MediaType
-#define MEDIA_TYPE_NAME "kind"
-#define MEDIA_TYPE_LIST \
-  ENUM_SUPPORTED(MEDIA_TYPE::MEDIA_TYPE_AUDIO, "audio") \
-  ENUM_SUPPORTED(MEDIA_TYPE::MEDIA_TYPE_VIDEO, "video") \
-  ENUM_SUPPORTED(MEDIA_TYPE::MEDIA_TYPE_DATA, "data")
+#define CRICKET_MEDIA_TYPE cricket::MediaType
+#define CRICKET_MEDIA_TYPE_NAME "kind"
+#define CRICKET_MEDIA_TYPE_LIST \
+  ENUM_SUPPORTED(CRICKET_MEDIA_TYPE::MEDIA_TYPE_AUDIO, "audio") \
+  ENUM_SUPPORTED(CRICKET_MEDIA_TYPE::MEDIA_TYPE_VIDEO, "video") \
+  ENUM_SUPPORTED(CRICKET_MEDIA_TYPE::MEDIA_TYPE_DATA, "data")
 
-#define ENUM(X) MEDIA_TYPE ## X
+#define ENUM(X) CRICKET_MEDIA_TYPE ## X
 #include "src/enums/macros/decls.h"
 #undef ENUM
