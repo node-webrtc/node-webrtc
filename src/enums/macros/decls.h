0fe572f45c675469bd364778777533372d8dd32d
Mark Roberts <markandrusroberts@gmail.com>
2019-04-02 15:03:02 +0800

    Add N-API enum converters

--- src/enums/macros/decls.h
+++ src/enums/macros/decls.h
@@ -1,6 +1,7 @@
 #ifdef ENUM
 
 #include "src/converters.h"
+#include "src/converters/napi.h"
 #include "src/converters/v8.h"
 
 namespace node_webrtc {
@@ -8,6 +9,7 @@ namespace node_webrtc {
 DECLARE_CONVERTER(ENUM(), std::string)
 DECLARE_CONVERTER(std::string, ENUM())
 DECLARE_TO_AND_FROM_JS(ENUM())
+DECLARE_TO_AND_FROM_NAPI(ENUM())
 
 }  // namespace node_webrtc
 
