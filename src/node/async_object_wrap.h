a52fe06323c34f69e5aa2ac0789d879578f5dada
Mark Roberts <markandrusroberts@gmail.com>
2019-04-29 16:09:03 -0700

    Try working around that crash

--- src/node/async_object_wrap.h
+++ src/node/async_object_wrap.h
@@ -4,6 +4,7 @@
 #include <mutex>
 
 #include <node-addon-api/napi.h>
+#include <v8.h>
 
 namespace node_webrtc {
 
@@ -16,8 +17,24 @@ class AsyncObjectWrap: public Napi::ObjectWrap<T> {
   void DestroyAsyncContext() {
     _async_context_mutex.lock();
     if (_async_context) {
-      Napi::HandleScope scope(this->Env());
-      delete _async_context;
+      // NOTE(mroberts): This is a hack and it sucks, but at least we
+      //
+      //   1. Avoid a memory leak
+      //   2. Avoid a crash
+      //
+      auto isolate = v8::Isolate::GetCurrent();
+      if (isolate) {
+        auto callingContext = isolate->GetCallingContext();
+        auto currentContext = isolate->GetCurrentContext();
+        if (currentContext.IsEmpty() && !callingContext.IsEmpty()) {
+          callingContext->Enter();
+        }
+        Napi::HandleScope scope(this->Env());
+        delete _async_context;
+        if (currentContext.IsEmpty()) {
+          callingContext->Exit();
+        }
+      }
       _async_context = nullptr;
     }
     _async_context_mutex.unlock();
