373852773dced7639953e0f74d71e121fef6224b
Mark Roberts <markandrusroberts@gmail.com>
2019-04-30 13:35:11 -0700

    Separate develop/publish package.json files

---
+++ scripts/download-prebuilt.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+/* eslint no-process-env:0, no-process-exit:0 */
+'use strict';
+
+const { spawnSync } = require('child_process');
+
+function main() {
+  const args = ['install'];
+
+  if (process.env.DEBUG) {
+    args.push('--debug');
+  }
+
+  if (process.env.TARGET_ARCH) {
+    args.push('--target_arch=' + process.env.TARGET_ARCH);
+  }
+
+  let { status } = spawnSync('node-pre-gyp', args, {
+    shell: true,
+    stdio: 'inherit'
+  });
+  if (status) {
+    process.exit(1);
+  }
+}
+
+main();
