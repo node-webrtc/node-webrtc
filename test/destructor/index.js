8f5d7ffefa849faf7d689d29862c115318690852
Mark Roberts <markandrusroberts@gmail.com>
2019-09-08 16:50:06 +0200

    Improve destructor tests (#553)

--- test/destructor/index.js
+++ test/destructor/index.js
@@ -2,94 +2,191 @@
 
 const test = require('tape');
 
-const { MediaStream, RTCPeerConnection, RTCSessionDescription } = require('../..');
+const {
+  RTCPeerConnection,
+  getUserMedia
+} = require('../..');
 
-const checkDestructor = require('./util');
+const {
+  RTCAudioSink,
+  RTCAudioSource,
+  RTCVideoSink,
+  RTCVideoSource,
+} = require('../..').nonstandard;
 
-const sdp = [
-  'v=0',
-  'o=- 0 1 IN IP4 0.0.0.0',
-  's=-',
-  't=0 0',
-  'a=group:BUNDLE audio',
-  'a=msid-semantic:WMS *',
-  'a=ice-ufrag:0000',
-  'a=ice-pwd:0000000000000000000000',
-  'a=fingerprint:sha-256 00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00',
-  'm=audio 9 UDP/TLS/RTP/SAVPF 109 9 0 8 101',
-  'c=IN IP4 0.0.0.0',
-  'a=mid:audio',
-  'a=sendrecv',
-  'a=setup:actpass',
-  'a=rtpmap:109 opus/48000/2',
-  'a=rtpmap:9 G722/8000/1',
-  'a=rtpmap:0 PCMU/8000',
-  'a=rtpmap:8 PCMA/8000',
-  'a=rtpmap:101 PCMA/16000',
-  'a=rtcp-mux',
-  'a=ssrc:1 cname:0',
-  'a=ssrc:1 msid:stream 123',
-].join('\r\n') + '\r\n';
+const { negotiateRTCPeerConnections } = require('../lib/pc');
+const { createDeferred, trackDestructors } = require('./util');
+
+async function waitUntilOpen(dataChannel) {
+  if (dataChannel.readyState === 'open') {
+    return;
+  }
+  await new Promise(resolve => {
+    dataChannel.addEventListener('open', function onopen() {
+      dataChannel.removeEventListener('open', onopen);
+      resolve();
+    });
+  });
+}
+
+async function setupRTCDataChannels() {
+  let dc1;
+  const dc2Deferred = createDeferred();
+
+  const [pc1, pc2] = await negotiateRTCPeerConnections({
+    withPc1(pc1) {
+      dc1 = pc1.createDataChannel('test');
+    },
+    withPc2(pc2) {
+      pc2.addEventListener('datachannel', function ondatachannel({ channel }) {
+        pc2.removeEventListener('datachannel', ondatachannel);
+        dc2Deferred.resolve(channel);
+      });
+    }
+  });
+
+  const dc2 = await dc2Deferred.promise;
+  await Promise.all([
+    waitUntilOpen(dc1),
+    waitUntilOpen(dc2)
+  ]);
+
+  return {
+    pc1,
+    pc2,
+    dc1,
+    dc2
+  };
+}
 
 test('RTCPeerConnection\'s destructor fires', async t => {
-  t.plan(1);
-  await checkDestructor(
-    'RTCPeerConnection',
-    () => new RTCPeerConnection(),
-    pc => pc.close());
-  t.pass();
+  const { destructor, stop } = trackDestructors();
+
+  await (() => {
+    const pc = new RTCPeerConnection();
+    pc.close();
+    return destructor(pc._pc);
+  })();
+
+  stop();
+  t.end();
 });
 
-// NOTE(mroberts): We can test RTCRtpReceivers and remote MediaStreamTracks in
-// the same way.
-[
-  ['RTCRtpReceiver', 'RTCRtpReceiver'],
-  ['MediaStreamTrack', 'remote MediaStreamTrack']
-].forEach(([type, description]) => {
-  test(`${description}'s destructor fires`, async t => {
-    t.plan(2);
-    await checkDestructor(
-      type,
-      async () => {
-        const pc = new RTCPeerConnection();
-        const offer = new RTCSessionDescription({ type: 'offer', sdp });
-        await pc.setRemoteDescription(offer);
-        t.equal(pc.getReceivers().length, 1);
-        return pc;
-      },
-      pc => pc.close());
-    t.pass();
-  });
+test('Destructors fire in RTCDataChannel use-case', async t => {
+  const { destructor, stop } = trackDestructors();
+
+  await (async () => {
+    const { pc1, pc2, dc1, dc2 } = await setupRTCDataChannels();
+
+    const destructors = [
+      destructor(pc1._pc),
+      destructor(pc2._pc),
+      destructor(dc1),
+      destructor(dc2),
+      destructor(pc1.sctp),
+      destructor(pc2.sctp),
+      destructor(pc1.sctp.transport),
+      destructor(pc2.sctp.transport),
+      destructor(pc1.sctp.transport.iceTransport),
+      destructor(pc2.sctp.transport.iceTransport)
+    ];
+
+    pc1.close();
+    pc2.close();
+
+    return Promise.all(destructors);
+  })();
+
+  stop();
+  t.end();
 });
 
-test('RTCRtpSender\'s destructor fires', async t => {
-  t.plan(2);
-  await checkDestructor(
-    'RTCRtpSender',
-    async () => {
-      const pc = new RTCPeerConnection();
-      const offer = new RTCSessionDescription({ type: 'offer', sdp });
-      await pc.setRemoteDescription(offer);
-      pc.getReceivers().forEach(receiver => pc.addTrack(receiver.track, new MediaStream()));
-      t.equal(pc.getSenders().length, pc.getReceivers().length);
-      return pc;
-    },
-    pc => pc.close());
-  t.pass();
+test('Destructors fire in MediaStreamTrack use-case', async t => {
+  const { destructor, stop } = trackDestructors();
+
+  await (async () => {
+    const stream1 = await getUserMedia({ audio: true, video: true });
+
+    const [pc1, pc2] = await negotiateRTCPeerConnections({
+      withPc1(pc1) {
+        stream1.getTracks().forEach(track => pc1.addTrack(track, stream1));
+      }
+    });
+
+    const localTracks = stream1.getTracks().map(destructor);
+    const remoteTracks = pc2.getReceivers().map(receiver => destructor(receiver.track));
+    const senders = pc1.getSenders().concat(pc2.getSenders()).map(destructor);
+    const receivers = pc1.getReceivers().concat(pc2.getReceivers()).map(destructor);
+    const transceivers = pc1.getTransceivers().concat(pc2.getTransceivers()).map(destructor);
+
+    const dtlsTransports = pc1.getReceivers().concat(pc2.getReceivers()).map(receiver => receiver.transport);
+    const iceTransports = dtlsTransports.map(dtlsTransport => dtlsTransport.iceTransport);
+
+    const destructors = [
+      destructor(pc1._pc),
+      destructor(pc2._pc)
+    ].concat(
+      localTracks
+    ).concat(
+      remoteTracks
+    ).concat(
+      senders
+    ).concat(
+      receivers
+    ).concat(
+      transceivers
+    ).concat(
+      dtlsTransports.map(destructor)
+    ).concat(
+      iceTransports.map(destructor)
+    );
+
+    stream1.getTracks().forEach(track => track.stop());
+    pc1.close();
+    pc2.close();
+
+    return Promise.all(destructors);
+  })();
+
+  stop();
+  t.end();
 });
 
-test('RTCDataChannel\'s destructor fires', async t => {
-  t.plan(1);
-  await checkDestructor(
-    'RTCDataChannel',
-    () => {
-      const pc = new RTCPeerConnection();
-      const dc = pc.createDataChannel('test');
-      // TODO(mroberts): It shouldn't be necessary to have to call `close`;
-      // closing the RTCPeerConnection should be sufficient.
-      dc.close();
-      return pc;
-    },
-    pc => pc.close());
-  t.pass();
+async function testSink(kind, t) {
+  const { destructor, stop } = trackDestructors();
+
+  const Source = {
+    audio: RTCAudioSource,
+    video: RTCVideoSource
+  }[kind];
+
+  const Sink = {
+    audio: RTCAudioSink,
+    video: RTCVideoSink
+  }[kind];
+
+  await (() => {
+    const source = new Source();
+    const track = source.createTrack();
+    const sink = new Sink(track);
+
+    track.stop();
+    sink.stop();
+
+    return Promise.all([
+      destructor(track),
+      destructor(sink),
+    ]);
+  })();
+
+  stop();
+  t.end();
+}
+
+test('RTCAudioSink\'s destructor fires', t => {
+  testSink('audio', t);
+});
+
+test('RTCVideoSink\'s destructor fires', t => {
+  testSink('video', t);
 });
