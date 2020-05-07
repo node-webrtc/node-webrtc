e8f0d07fef3e4cc65d6db617b864f054fdc37d67
Arlo Breault <arlolra@gmail.com>
2020-05-07 14:57:23 -0400

    Don't signal iceGatheringState complete if connection closed
    This only happens internal to node-webrtc,
    https://chromium.googlesource.com/external/webrtc/+/branch-heads/m79/pc/peer_connection.cc#4385
    
    Fixes #624

--- test/closing-peer-connection.js
+++ test/closing-peer-connection.js
@@ -59,3 +59,20 @@ test('make sure channel is available after after connection is closed on the oth
       peer1.setRemoteDescription(peer2.localDescription);
     });
 });
+
+
+test('make sure onicecandidate handler doesn\'t fire when connection is closed', function(t) {
+  t.plan(1);
+
+  var peer = new RTCPeerConnection({ iceServers: [] });
+
+  pc.onicecandidate = function () {
+    t.ok(false, 'should not have fired');
+  };
+
+  pc.close();
+
+  setTimeout(function() {
+    t.ok(true, 'other t did not fire');
+  }, 100);
+});
