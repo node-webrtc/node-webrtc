1d3bce470d732f429057559cb5e6e740486c7296
Shachar Itzhaky <corwin.amber@gmail.com>
2021-11-07 18:25:10 +0200

    [build] Tweak - do not link with c++abi on macOS.
    Seems to be not needed. Hopefully this does not break the build on Linux :P
    Also, iostream seems to crash Node!
    Removed from build as it is not needed anyway...

--- scripts/build-webrtc.sh
+++ scripts/build-webrtc.sh
@@ -7,10 +7,11 @@ set -v
 export PATH=$DEPOT_TOOLS:$PATH
 
 export TARGETS="webrtc libjingle_peerconnection"
-if [ "$(uname)" == "Linux" ]; then
-if [[ "$TARGET_ARCH" == arm* ]]; then
+if [[ "$(uname)" == "Linux" && "$TARGET_ARCH" == arm* ]]; then
   export TARGETS="$TARGETS pc:peerconnection libc++ libc++abi"
 fi
+if [[ "$(uname)" == "Darwin" ]]; then
+  export TARGETS="$TARGETS libc++"
 fi
 
 if [ -z "$PARALLELISM" ]; then
