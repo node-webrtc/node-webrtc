2c5d3f81eae8eab501c7c13405c225eb9d32c9da
Shachar Itzhaky <corwin.amber@gmail.com>
2021-11-17 10:58:56 -0500

    [build] Facilitate use of custom libstc++ vs. system one.
    For g++ compilation.

--- scripts/pack-webrtc.sh
+++ scripts/pack-webrtc.sh
@@ -4,18 +4,23 @@ set -e
 
 set -v
 
-mkdir -p lib
-ar -r lib/libc++.a `find obj/buildtools/third_party/libc++{,abi} -name '*.o'`
-
 DL=$PWD/../../download
 SUBDIRS=$(echo webrtc/{api,rtc_base,modules,system_wrappers,p2p,common_video,common_audio,media,logging,call,pc})
 
+if [ -d obj/buildtools/third_party/libc++ ] ; then
+  mkdir -p lib
+  ar -r lib/libc++.a `find obj/buildtools/third_party/libc++{,abi} -name '*.o'`
+  LIBCXX=(
+    -C $PWD/lib libc++.a
+    -C $DL/src/buildtools/third_party/libc++/trunk/include .
+    -C $DL/src/buildtools/third_party/libc++ __config_site
+  )
+fi
+
 tar Jcf libwebrtc-bin.tar.xz \
-    -C $PWD/lib libc++.a \
+    ${LIBCXX} \
     -C $PWD/obj/pc libpeerconnection.a \
     -C $PWD/obj libwebrtc.a \
     -C $DL $(cd $DL && find $SUBDIRS -name '*.h') \
-    -C $DL/src/buildtools/third_party/libc++/trunk/include .  \
-    -C $DL/src/buildtools/third_party/libc++ __config_site \
     -C $DL/src/third_party/abseil-cpp $(cd $DL/src/third_party/abseil-cpp && find absl -name '*.h')  \
     -C $DL/src/third_party/libyuv/include $(cd $DL/src/third_party/libyuv/include && find . -name '*.h')
