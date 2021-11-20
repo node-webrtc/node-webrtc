9630c80abce6b76e472e0f0a809639ac26b67da7
Shachar Itzhaky <corwin.amber@gmail.com>
2021-11-20 18:15:23 +0200

    [build] Download phase 1 prebuilt if not present.
    Currently downloads from `github.com/corwin-of-amber`, as a
    showcase.

--- scripts/unpack-webrtc.sh
+++ scripts/unpack-webrtc.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+version=$(node -p "require('$(dirname $0)/../package.json').version")
 platform=$(uname -s | tr '[A-Z]' '[a-z]')
 arch=${TARGET_ARCH:-$(uname -m | tr '[A-Z]' '[a-z]')}
+
+asset=libwebrtc-${platform}-${arch}.tar.gz
+
+if [ -e $(dirname $0)/../phase-1/build/stage/${version}/${asset} ]; then
+  cp $(dirname $0)/../phase-1/build/stage/${version}/${asset} .
+fi
+
+BASE_URL=https://github.com/corwin-of-amber/node-webrtc/releases/download
 
 set -e
 
 set -v
 
-tar xf libwebrtc-${platform}-${arch}.tar.gz
+if [ ! -e ${asset} ]; then
+  URL=${BASE_URL}/${version}/${asset}
 
+  echo Downloading "${URL}"
+  wget "$URL"
+fi
+
+echo Unpacking "${asset}"
+tar xf "${asset}"
